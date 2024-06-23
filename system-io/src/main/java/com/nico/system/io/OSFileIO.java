b1bdc9dd19787a5f951a1e3052051f011b741843
nico <664949079@qq.com>
2024-06-23 08:57:32 +0800

    init-code

---
+++ system-io/src/main/java/com/nico/system/io/OSFileIO.java
@@ -0,0 +1,11 @@
+package com.nico.system.io;
+
+public class OSFileIO {
+
+    static byte[] data = "123456789\n".getBytes();
+    static String path = "/root/testfileio/out.txt";
+
+    public static void main(String[] args) {
+        switch ()
+    }
+}
